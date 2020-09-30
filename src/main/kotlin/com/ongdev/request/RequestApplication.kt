c3578b4bc9016839ba1f8bfce862a49dbb857c4c
Duy Nguyen <milonguyen95@outlook.com>
2020-09-30 22:50:33 +0700

    Missing files

---
+++ src/main/kotlin/com/ongdev/request/RequestApplication.kt
@@ -0,0 +1,11 @@
+package com.ongdev.request
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestApplication
+
+fun main(args: Array<String>) {
+    runApplication<RequestApplication>(*args)
+}
---
+++ src/test/kotlin/com/ongdev/request/RequestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ongdev.request
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
