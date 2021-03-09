41bb626540b73e8490ccd624ef5382f18b6f3f32
Ong Dev <admin@ongdev.com>
2021-03-09 09:40:25 +0700

    Update KeycloakDeployment

--- src/main/kotlin/com/ongdev/request/configs/KeycloakConfiguration.kt
+++ src/main/kotlin/com/ongdev/request/configs/KeycloakConfiguration.kt
@@ -1,12 +1,27 @@
 package com.ongdev.request.configs
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.KeycloakDeployment
+import org.keycloak.adapters.KeycloakDeploymentBuilder
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.representations.adapters.config.AdapterConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class KeycloakConfiguration {
     @Bean
     fun keycloakSpringBootConfigResolver()
             : KeycloakSpringBootConfigResolver = KeycloakSpringBootConfigResolver()
+
+    @Bean
+    fun keycloakDeployment(adapterConfig: AdapterConfig?): KeycloakDeployment? {
+        return KeycloakDeploymentBuilder.build(adapterConfig)
+    }
+
+    @Bean
+    fun keycloakConfigResolver(keycloakDeployment: KeycloakDeployment?): KeycloakConfigResolver? {
+        return KeycloakConfigResolver { keycloakDeployment }
+    }
 }
